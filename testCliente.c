8c3d12cc11ee1b3303913a76814fadb827afb742
Valentino <valentinodefelice@gmail.com>
2018-03-23 16:10:27 -0300

    Subiendo archivos

---
+++ testCliente.c
@@ -0,0 +1,68 @@
+#include <stdio.h>          
+#include <stdlib.h>          
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define MSGSEND "hola" 
+#define RCVBUFSIZE 1024
+
+void DieWithError(char * msg){
+	printf("%s\n", msg);
+	exit(-1);
+}
+
+void main() {
+
+int clientSock, servSock, recvMsgSize, clntLen, echoStringLen;
+struct sockaddr_in echoServAddr, echoClntAddr;
+
+char echoString[RCVBUFSIZE] = "hola";
+char echoBuffer[RCVBUFSIZE];
+
+echoServAddr.sin_family = AF_INET;
+//echoServAddr.sin_addr.s_addr = inet_pton("localhost");
+echoServAddr.sin_port = htons(1111);
+
+
+	if(inet_pton(AF_INET, "127.0.0.1", &echoServAddr.sin_addr) < 0)
+		DieWithError("inet_pton() failed");
+	printf("inet_pton\n");
+
+	if((clientSock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+		DieWithError("socket() failed");
+	printf("clientSock\n");
+
+	if(connect(clientSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+		DieWithError("connect() failed");
+	printf("connect\n");
+
+	if(send(clientSock, echoString, echoStringLen, 0) != echoStringLen)
+		DieWithError("send() sent a different number of the bytes than expected");
+	printf("send\n");
+
+	if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+		DieWithError("send() sent a different number of the bytes than expected");
+	printf("recv\n");
+
+	printf("%s\n", echoBuffer);
+
+	/*for (;;){
+		clntLen = sizeof(echoClntAddr);
+
+		//if(send(clientSock, MSGSEND, sizeof(MSGSEND), 0) != clntLen)
+		if(send(clientSock, MSGSEND, sizeof(MSGSEND), 0) != clntLen)
+			DieWithError("send() sent a different number of the bytes than expected");
+
+		if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+			DieWithError("recv() failed");
+
+		while (recvMsgSize < 0) {
+			if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
+				DieWithError("send() failed");
+			if ((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+				DieWithError("recv() failed");
+		}
+	} */
+
+close(clientSock);
+}
---
+++ testServer.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>          
+#include <stdio.h>          
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAXPENDING 4
+#define RCVBUFSIZE 1024
+
+void DieWithError(char * msg){
+	printf("%s\n", msg);
+	exit(-1);
+}
+
+void main() {
+
+int servSock, clientSock, recvMsgSize, clntLen;
+struct sockaddr_in echoServAddr, echoClntAddr;
+char echoBuffer[RCVBUFSIZE];
+int echoServPort = 1111;
+
+echoServAddr.sin_family = AF_INET;
+echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+echoServAddr.sin_port = htons(echoServPort);
+
+	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithError("socket() failed pepit");
+	printf("serverSock\n");
+
+	if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+		DieWithError("bind() failed");
+	printf("bind\n");
+
+	if (listen(servSock, MAXPENDING) < 0)
+		DieWithError("listen() failed");
+	printf("listen\n");
+
+	if((clientSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
+		DieWithError("accept() failed");
+	printf("accept\n");
+
+	if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) <0 )
+		DieWithError("recv() failed");
+	printf("recv\n");
+
+	while (recvMsgSize > 0){
+		if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
+			DieWithError("send() failed");
+	printf("send\n");
+		if ((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+			DieWithError("recv() failed");
+	printf("recv\n");
+	}
+
+
+	/*for(;;) {
+		clntLen = sizeof(echoClntAddr);
+
+		if((clientSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
+			DieWithError("accept() failed");
+
+		if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+			DieWithError("recv() failed");
+
+		while(recvMsgSize > 0) {
+			if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
+				DieWithError("send() failed");
+			if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+				DieWithError("recv() failed");
+		} */
+	close(clientSock);
+	return;
+
+}
