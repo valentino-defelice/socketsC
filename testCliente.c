99094d644776f5e42d31001d0b081491138ac42e
Valentino <valentinodefelice@gmail.com>
2018-03-29 18:55:56 -0300

    Carga inicial

--- testCliente.c
+++ testCliente.c
@@ -3,7 +3,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <string.h>
 #define MSGSEND "hola" 
+#define ENDCODE "\\r\\n" 
+#define USER "USER" 
 #define RCVBUFSIZE 1024
 
 void DieWithError(char * msg){
@@ -11,60 +14,72 @@ void DieWithError(char * msg){
 	exit(-1);
 }
 
-void main() {
+void main(int argc, char *argv[]) {
 
+unsigned long ip = 0;
 int clientSock, servSock, recvMsgSize, clntLen, echoStringLen;
 struct sockaddr_in echoServAddr, echoClntAddr;
+int port = 0;
 
-char echoString[RCVBUFSIZE];
+
 char echoBuffer[RCVBUFSIZE];
+char echoString[RCVBUFSIZE];
+char username[20];
+char password[20];
 
 echoServAddr.sin_family = AF_INET;
 //echoServAddr.sin_addr.s_addr = inet_pton("localhost");
-echoServAddr.sin_port = htons(1111);
 
+	if(argc != 3)
+		DieWithError("Ingrese IP y Puerto");
+    port = atoi(argv[2]);
+
+	if (0 == inet_pton(AF_INET, argv[1], &ip))
+        DieWithError("La IP no es correcta");
+
+    if((atoi(argv[2]) < 1) || (atoi(argv[2]) > 65000))
+    	DieWithError("Puerto Incorrecto, debe ser mayor a 1 y menor a 65000\n");
+    echoClntAddr.sin_port = htons(port);
+	echoServAddr.sin_port = htons(port);
 
 	if(inet_pton(AF_INET, "127.0.0.1", &echoServAddr.sin_addr) < 0)
 		DieWithError("inet_pton() failed");
-	printf("inet_pton\n");
 
 	if((clientSock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
 		DieWithError("socket() failed");
-	printf("clientSock\n");
 
 	if(connect(clientSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
 		DieWithError("connect() failed");
-	printf("connect\n");
 
-	for(;;){
-		printf("Yo: ");
-		fgets(echoString, sizeof(echoString), stdin);
-		
-		if(send(clientSock, echoString, RCVBUFSIZE, 0) < 0)
-			DieWithError("send() sent a different number of the bytes than expected");
-		printf("✓✓\n");
+	if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+		DieWithError("send() sent a different number of the bytes than expected");
+	printf("%s \n", echoBuffer);
 
-		if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-			DieWithError("send() sent a different number of the bytes than expected");
-		printf("Servidor: %s \n", echoBuffer);
+	//PEDIMOS USUARIO, FORMATEAMOS MENSAJE Y ENVIAMOS
+	printf("username: ");
+	gets(username);
+	snprintf(echoString, sizeof(echoString), "%s%s%s", "USER ", username, " \\r\\n"); //FORMATEADO DE MSJ
+
+	send(clientSock, echoString, sizeof(echoString), 0);	//ENVIAMOS EL MSJ
+
+	//COMPROBAMOS SI NECESITA PASSWORD
+	if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+		DieWithError("send() sent a different number of the bytes than expected");
+
+	//comparando
+	char* data = strtok(echoBuffer, " ");
+	char datos[20];
+	strcpy(datos, data);
+
+	if(strcmp(datos, "331") == 0){
+		printf("password: ");
+		gets(password);
+		snprintf(echoString, sizeof(echoString), "%s%s%s", "PASS ", password, " \\r\\n"); //FORMATEADO DE MSJ
+
+		send(clientSock, echoString, sizeof(echoString), 0);	//ENVIAMOS EL MSJ
+		printf("%s\n", echoString);
 	}
-	/*for (;;){
-		clntLen = sizeof(echoClntAddr);
 
-		//if(send(clientSock, MSGSEND, sizeof(MSGSEND), 0) != clntLen)
-		if(send(clientSock, MSGSEND, sizeof(MSGSEND), 0) != clntLen)
-			DieWithError("send() sent a different number of the bytes than expected");
-
-		if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-			DieWithError("recv() failed");
-
-		while (recvMsgSize < 0) {
-			if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-				DieWithError("send() failed");
-			if ((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-				DieWithError("recv() failed");
-		}
-	} */
 
 close(clientSock);
 }
--- testServer.c
+++ testServer.c
@@ -1,26 +1,45 @@
 #include <stdlib.h>          
+#include <string.h>          
 #include <stdio.h>          
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #define MAXPENDING 4
+#define USER "USER"
 #define RCVBUFSIZE 1024
+#define HELLOSRV "220 srvFtp version 1.0\r\n"
+#define PWRREQ "331 Password required for \r\n"
 
 void DieWithError(char * msg){
 	printf("%s\n", msg);
 	exit(-1);
 }
 
-void main() {
+void main(int argc, char *argv[]) {
 
+FILE *fp;
 int servSock, clientSock, recvMsgSize, clntLen;
 struct sockaddr_in echoServAddr, echoClntAddr;
 char echoBuffer[RCVBUFSIZE];
-int echoServPort = 1111;
+char echoString[RCVBUFSIZE];
+int port = 0;
+
+char usuarioRecibido[20];
+char passwordRecibido[20];
+
+char usuario[3][10];
+char password[3][10];
 
 echoServAddr.sin_family = AF_INET;
 echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-echoServAddr.sin_port = htons(echoServPort);
+
+	if(argc != 2)
+		DieWithError("Ingrese un Puerto");
+	port = atoi(argv[1]);
+
+	if((port < 1) || (port > 65000))
+		DieWithError("Puerto Incorrecto, debe ser mayor a 1 y menor a 65000\n");
+	echoServAddr.sin_port = htons(port);
 
 	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		DieWithError("socket() failed pepit");
@@ -34,51 +53,93 @@ echoServAddr.sin_port = htons(echoServPort);
 		DieWithError("listen() failed");
 	printf("listen\n");
 
-	/*while (recvMsgSize > 0){
-		if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-			DieWithError("send() failed");
-	printf("send\n");
-		if ((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-			DieWithError("recv() failed");
-	printf("recv\n");
-	}*/
+	if((clientSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
+		DieWithError("accept() failed");
+		printf("accept\n");
+
+	send(clientSock, HELLOSRV, sizeof(HELLOSRV), 0);
+
+	if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+			DieWithError("send() sent a different number of the bytes than expected");
 
 
-	for(;;) {
-		clntLen = sizeof(echoClntAddr);
+	//comparando
+	char* data = strtok(echoBuffer, " ");
+	char datos[20];
+	strcpy(datos, data);
 
-		if((clientSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
-			DieWithError("accept() failed");
-			printf("accept\n");
-
-		if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-			DieWithError("recv() failed");
-			printf("recv\n");
-			
-		/* while(recvMsgSize > 0) {
-			printf("Cliente: %s\n",echoBuffer);
-			printf("Yo: \n");
-			fgets(echoBuffer, sizeof(echoBuffer), stdin);
-
-			if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-				DieWithError("send() failed");
-				printf("send\n");
-			if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-				DieWithError("recv() failed");
-		} */
-		for(;;) {
-			printf("Cliente: %s\n",echoBuffer);
-			printf("Yo: ");
-			fgets(echoBuffer, sizeof(echoBuffer), stdin);
-
-			if (send(clientSock, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-				DieWithError("send() failed");
-				printf("send\n");
-			if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
-				DieWithError("recv() failed");
-		} 
-	close(clientSock);
+	if(strcmp(datos, "USER") != 0){
+		printf("Error, codigo recibido: %s\n", datos);
+		return -1;
+	} 
+	else {
+		data = strtok(NULL, " ");
+		strcpy(usuarioRecibido, data);
+		printf("Usuario recibido: %s\n", usuarioRecibido);
 	}
-	return;
 
+	// ABRIENDO Y COPIANDO ARCHIVO
+	char buff[100];
+	fp = fopen("usuarios.txt", "r");
+
+		int i = 0;
+		char* lectura;	//DONDE GUARDAREMOS LOS VALORES OBTENIDOS DEL STRTOK(Usuario y Contraseña)
+		fscanf(fp, "%s", buff);
+
+		while(!feof(fp)) {
+		
+		lectura = strtok(buff, ":"); //LEEMOS Y GUARDAMOS EL PRIMER USUARIO
+			strcpy(usuario[i], lectura);
+
+		lectura = strtok(NULL, ":"); //LEEMOS Y GUARDAMOS EL PRIMER USUARIO
+			strcpy(password[i], lectura);
+		
+		fscanf(fp, "%s", buff); //LEEMOS NUEVA LINEA DEL ARCHIVO
+		
+		i++; 
+		}
+
+	//COMPROBANDO USUARIO
+		for(i = 0; i < 3; i++) {
+	        if(strcmp(usuarioRecibido, usuario[i]) == 0){
+	        	snprintf(echoString, sizeof(echoString), "%s%s%s", "331 Password required for: ", usuarioRecibido, " \\r\\n");
+	        	send(clientSock, echoString, sizeof(echoString), 0);
+	        	break;
+        	} 
+        	else {
+	        	snprintf(echoString, sizeof(echoString), "%s%s%s", "530 Login incorrect for: ", usuarioRecibido, " \\r\\n");
+	        	send(clientSock, echoString, sizeof(echoString), 0);
+	        }
+	    }
+
+		// RECIBIMOS PASSWORD
+	    if((recvMsgSize = recv(clientSock, echoBuffer, RCVBUFSIZE, 0)) < 0)
+	    	DieWithError("send() sent a different number of the bytes than expected");
+
+	    data = strtok(echoBuffer, " ");
+	    strcpy(datos, data);
+
+	    // COMPARANDO CODIGO PASSWORD
+	    if(strcmp(datos, "PASS") != 0){
+	    	printf("Error, codigo recibido: %s\n", datos);
+	    	return -1;
+	    } else {
+	    	data = strtok(NULL, " ");
+	    	strcpy(passwordRecibido, data);
+	    				for(i = 0; i < 3; i++) {
+	    			        if(strcmp(passwordRecibido, password[i]) == 0){
+	    			        	snprintf(echoString, sizeof(echoString), "%s%s%s", "230 User: ", usuarioRecibido[i], "logged in \\r\\n");
+	    			        	send(clientSock, echoString, sizeof(echoString), 0);
+	    			        	break;
+	    		        	} 
+	    		        	else {
+	    			        	snprintf(echoString, sizeof(echoString), "%s%s%s", "530 Password incorrect for: ", usuarioRecibido[i], " \\r\\n");
+	    			        	send(clientSock, echoString, sizeof(echoString), 0);
+	    			        }
+	    			    }
+	    }
+
+
+	close(clientSock);
+	return;
 }
